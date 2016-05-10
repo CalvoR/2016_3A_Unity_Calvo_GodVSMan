1f0642ddd9f5a02c0c8d61ab05c8557dfd83665e
clement_m <clement.mezelle@free.fr>
2016-05-10 18:17:01 +0200

    ALPHA 1.5 Branches cleanées (avec Xavier)

--- Unity/Assets/Scripts/God/Spells/gvmSpellEffectGetter.cs
+++ Unity/Assets/Scripts/God/Spells/gvmSpellEffectGetter.cs
@@ -41,10 +41,6 @@ public class gvmSpellEffectGetter : MonoBehaviour {
             data.HP += prop.damage;
         }
         Debug.Log(effect);
-<<<<<<< HEAD
-        effectList.RemoveAt(effectIndex);
-=======
         effectList.Remove(effect);
->>>>>>> origin/master
     }
 }
