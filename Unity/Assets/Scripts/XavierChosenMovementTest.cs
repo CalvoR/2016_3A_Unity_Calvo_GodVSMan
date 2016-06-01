86e6388fcaaff15494b8f410df1eb95db3714070
krrom@live.fr <krrom>
2016-06-01 19:02:34 +0200

    Beta with network

--- Unity/Assets/Scripts/XavierChosenMovementTest.cs
+++ Unity/Assets/Scripts/XavierChosenMovementTest.cs
@@ -13,20 +13,18 @@ public class XavierChosenMovementTest : NetworkBehaviour {
     [SerializeField]
     GameObject playerCharacter;
 
-    [SerializeField]
-    Collider terrainCollider;
-
     public override void OnStartLocalPlayer() {
         base.OnStartLocalPlayer();
         if (Camera.main && Camera.main.gameObject) {
             Camera.main.gameObject.SetActive(false);
         }
-        playerCamera.gameObject.SetActive(true);
+        playerCamera.enabled = true;
     }
 
     public void FixedUpdate() {
         if (isLocalPlayer) {
             CmdStraf(Input.GetAxis("Sideway"));
+            CmdMoveForward(Input.GetAxis("Forward"));
             //GoToPositionFromClick(mousePosition);
         }
     }
@@ -35,31 +33,9 @@ public class XavierChosenMovementTest : NetworkBehaviour {
     public void CmdStraf(float strafSpeed) {
         playerCharacter.transform.position += Vector3.right * speed * strafSpeed;
     }
-    /*
-    public void GoToPositionFromClick(Vector3 mousePosition) {
-        var ray = playerCamera.ScreenPointToRay(mousePosition);
-
-        RaycastHit hit;
-
-        if (terrainCollider.Raycast(ray, out hit, 2000f)) {
-            CmdGoToPosition(hit.point);
-        }
-    }
 
     [Command]
-    public void CmdGoToPosition(Vector3 pos) {
-        // For Client Action Replication Prediction
-        RpcGoToPosition(pos);
-
-        // If the server is a host do not apply twice the action due to prediction
-        if (!Network.isClient) {
-            agent.SetDestination(pos);
-        }
+    public void CmdMoveForward(float strafSpeed) {
+        playerCharacter.transform.position += Vector3.forward * speed * strafSpeed;
     }
-
-    // For Client Action Replication Prediction
-    [ClientRpc]
-    public void RpcGoToPosition(Vector3 pos) {
-        agent.SetDestination(pos);
-    }*/
 }
