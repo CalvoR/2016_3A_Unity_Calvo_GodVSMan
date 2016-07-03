f5d21a53a2984869bfea72f079d73b851e937169
clement_m <clement.mezelle@free.fr>
2016-07-03 23:55:05 +0200

    Remise à niveau déplacement élu + orientation caméra ELU (+ réarrangement dossiers du projet)

---
+++ Unity/Assets/Scripts/Elu/gvmPitchYawCamera.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class gvmPitchYawCamera : NetworkBehaviour {
+
+    #region Attributs
+
+    [SerializeField]
+    Transform CharacterTransform;
+
+    [SerializeField]
+    Transform HeadTransform;
+
+    [SerializeField]
+    float pitchSpeed;
+
+    [SerializeField]
+    float yawSpeed;
+
+    private float xVariation;
+    private float yVariation;
+    private const float CLAMP_ANGLE = 75.0f;
+
+    #endregion
+
+    #region Méthodes
+
+    void Start()
+    {
+        yVariation = HeadTransform.rotation.eulerAngles.x;
+        xVariation = 0.0f;
+    }
+    
+
+	void Update ()
+    {
+        yVariation += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * pitchSpeed;
+        yVariation = Mathf.Clamp(yVariation, -CLAMP_ANGLE, CLAMP_ANGLE);
+
+        xVariation = Input.GetAxisRaw("Mouse X");
+    }
+
+
+    void FixedUpdate()  
+    {
+        CmdRotateCamera();   
+    }
+
+    [Command]
+    void CmdRotateCamera()
+    {
+        HeadTransform.rotation = Quaternion.Euler(-yVariation, HeadTransform.rotation.eulerAngles.y, 0.0f);
+        CharacterTransform.Rotate(Vector3.up * xVariation * Time.deltaTime * yawSpeed);
+    }
+
+    #endregion
+
+}
--- Unity/Assets/Scripts/Elu/gvmYawMainCamera.cs
+++ Unity/Assets/Scripts/Elu/gvmYawMainCamera.cs
@@ -1,65 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.Networking;
 
-public class gvmYawMainCamera : NetworkBehaviour {
+public class gvmYawMainCamera : MonoBehaviour {
 
+    [SerializeField]
+    Transform cameraTransform;
 
     [SerializeField]
     float yawSpeed;
-    [SerializeField]
-    float pitchSpeed;
-    
+
     private float xVariation;
-    private float yVariation;
-    private float clampAngle = 75.0f;
-    
-    [SerializeField]
-    float speed;
 
-    [SerializeField]
-    GameObject cameraTransform;
-    [SerializeField]
-    GameObject playerCharacter;
-    
-    // Update is called once per frame
-    void FixedUpdate() {
-        if (isLocalPlayer) {
-            xVariation = Input.GetAxisRaw("Mouse X") * Time.deltaTime * yawSpeed;
-
-            yVariation += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * pitchSpeed;
-            yVariation = Mathf.Clamp(yVariation, -clampAngle, clampAngle);
-
-            CmdMoveHead(yVariation);
-            CmdMoveBody(xVariation);
-
-            CmdStraf(Input.GetAxis("Sideway"));
-            CmdMoveForward(Input.GetAxis("Forward"));
-        }
-    }
-    
-    [Command]
-    public void CmdMoveHead( float y) {
-        cameraTransform.transform.Rotate(Vector3.right, y);
-        //cameraTransform.rotation = Quaternion.Euler(-y, cameraTransform.rotation.eulerAngles.y, 0.0f);
+	
+	// Update is called once per frame
+	void Update () 
+    {
+            xVariation = Input.GetAxisRaw("Mouse X");
+        
     }
 
-    [Command]
-    public void CmdMoveBody(float x) {
-        playerCharacter.transform.Rotate(Vector3.up * x);
-    }
 
-    [Command]
-    public void CmdStraf(float strafSpeed) {
-        playerCharacter.transform.Translate(Vector3.right * speed * strafSpeed);
-    }
-
-    [Command]
-    public void CmdMoveForward(float strafSpeed) {
-        playerCharacter.transform.Translate(Vector3.forward * speed * strafSpeed);
-        playerCharacter.transform.position =
-            Vector3.right * playerCharacter.transform.position.x +
-            Vector3.up +
-            Vector3.forward * playerCharacter.transform.position.z;
+    void FixedUpdate()  
+    {
+            cameraTransform.Rotate(Vector3.up * xVariation * Time.deltaTime * yawSpeed);
+        
     }
 }
